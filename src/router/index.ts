1b59f7734f5a0db652bce7c579483ddc7a9e176e
Alan Nascimento <alan.silva.n@hotmail.com>
2020-06-18 14:44:45 -0300

    feat: create register component

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,8 @@
 import Vue from 'vue';
 import VueRouter, { RouteConfig } from 'vue-router';
 
-import Home from '../views/Home.vue';
-import Managers from '../views/Managers.vue';
+import Home from '@/views/Home.vue';
+import Managers from '@/views/Managers.vue';
 
 Vue.use(VueRouter);
 
@@ -18,12 +18,10 @@ const routes: Array<RouteConfig> = [
     component: Managers,
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    // component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    path: '/register',
+    name: 'Register',
+    // component: Register,
+    component: () => import('@/views/Register.vue'),
   },
 ];
 
---
+++ src/views/Register.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="container">
+    <h1>Register</h1>
+
+    <form @submit.prevent="onSubmit">
+      <div class="form-group">
+        <label for="name">Name</label>
+        <input type="text" class="form-control" v-model="user.name" />
+      </div>
+      <div class="form-group">
+        <label for="name">E-mail</label>
+        <input type="text" class="form-control" v-model="user.email" />
+      </div>
+      <div class="form-group">
+        <label for="name">Password</label>
+        <input type="password" class="form-control" v-model="user.password" />
+      </div>
+
+      <button class="btn btn-primary">Save</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    user: {
+      name: '',
+      email: '',
+      password: ''
+    }
+  }),
+  methods: {
+    onSubmit() {
+      console.warn(this.user);
+    }
+  }
+};
+</script>
